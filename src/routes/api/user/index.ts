e7eb47a5e195c8676c5a36dc862369a4d7d7ecee
Rodrigo Limões <rolimoes@gmail.com>
2023-02-26 01:04:58 -0300

    Validating the "body" in the user creation endpoint

--- src/routes/api/user/index.ts
+++ src/routes/api/user/index.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-
+import { validate } from '@common/utils/validate.utils';
 const router = Router();
 
 // controller
 import { userController } from '@src/user';
 
-router.get('/', userController.create);
+// dtos
+import { UserDto } from '@user/dtos/user.dto';
+
+// validate
+const validateBody = validate('body');
+
+router.post('/', validateBody(UserDto), userController.create);
 
 export default router;
