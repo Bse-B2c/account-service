098e0ab5e9b9131806a6628297561d8aaa497a45
Rodrigo Limões <rolimoes@gmail.com>
2023-02-26 20:53:05 -0300

    Adding address service; Adding create function

---
+++ src/address/address.service.ts
@@ -0,0 +1,35 @@
+import { AddressService as Service } from '@address/interfaces/addressService.interface';
+import { Address } from '@address/entity/address.entity';
+import { AddressDto } from '@address/dtos/address.dto';
+import { Repository } from 'typeorm';
+import { UserService } from '@user/interfaces/userService.interface';
+import { HttpException, HttpStatusCode } from '@bse-b2c/common';
+
+export class AddressService implements Service {
+	constructor(
+		private repository: Repository<Address>,
+		private userService: UserService
+	) {}
+
+	create = async (address: AddressDto): Promise<Address> => {
+		const user = await this.userService.findOne(address.userId);
+		const existsAddress = user.addresses.find(
+			({ streetName }) =>
+				streetName.toLowerCase() === address.streetName.toLowerCase()
+		);
+
+		if (existsAddress)
+			throw new HttpException({
+				statusCode: HttpStatusCode.CONFLICT,
+				message: `Address "${address.streetName}" already exists`,
+			});
+
+		const newAddress = this.repository.create({
+			...address,
+			user: user,
+			active: user.addresses?.length <= 0,
+		});
+
+		return this.repository.save(newAddress);
+	};
+}
