32710257d1ef70b2fba115a75d9da7062164a133
Rodrigo Limões <rolimoes@gmail.com>
2023-02-26 20:52:38 -0300

    Adding address service interface

---
+++ src/address/interfaces/addressService.interface.ts
@@ -0,0 +1,6 @@
+import { AddressDto } from '@address/dtos/address.dto';
+import { Address } from '@address/entity/address.entity';
+
+export interface AddressService {
+	create(address: AddressDto): Promise<Address>;
+}
