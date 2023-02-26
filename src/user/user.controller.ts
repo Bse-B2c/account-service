1e538927b63e9d05dafb77f2df74b9bc7f9b867b
Rodrigo Limões <rolimoes@gmail.com>
2023-02-26 01:06:45 -0300

    Adding user service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,29 @@
 import { Request, Response, NextFunction } from 'express';
 import { HttpStatusCode } from '@bse-b2c/common';
+import { UserService } from '@user/interfaces/userService.interface';
+import { UserDto } from '@user/dtos/user.dto';
 
 export class UserController {
-	create = (req: Request, res: Response, next: NextFunction) => {
+	constructor(private service: UserService) {}
+
+	create = async (req: Request, res: Response, next: NextFunction) => {
 		try {
+			const { name, email, password, phone, cpf, brithDate } =
+				req.body as UserDto;
+
+			const response = await this.service.create({
+				name,
+				email,
+				password,
+				phone,
+				cpf,
+				brithDate,
+			});
+
 			return res.status(HttpStatusCode.CREATED).send({
 				statusCode: HttpStatusCode.CREATED,
 				error: null,
-				data: {},
+				data: response,
 			});
 		} catch (e) {
 			next(e);
---
+++ src/user/user.service.ts
@@ -0,0 +1,41 @@
+import { UserService as Service } from '@user/interfaces/userService.interface';
+import { PasswordUtils } from '@common/utils/password.utils';
+import { UserDto } from '@user/dtos/user.dto';
+import { User } from '@user/entity/user.entity';
+import { Repository } from 'typeorm';
+import { HttpException, HttpStatusCode } from '@bse-b2c/common';
+
+export class UserService implements Service {
+	constructor(
+		private repository: Repository<User>,
+		private passwordUtils: PasswordUtils
+	) {}
+
+	create = async ({
+		name,
+		password,
+		email,
+		brithDate,
+		cpf,
+		phone,
+	}: UserDto): Promise<User> => {
+		const user = await this.repository.findOne({ where: { email } });
+
+		if (user)
+			throw new HttpException({
+				statusCode: HttpStatusCode.CONFLICT,
+				message: `The user already exists.`,
+			});
+
+		const newUser = this.repository.create({
+			name,
+			email,
+			cpf,
+			phone,
+			password: this.passwordUtils.generate(password),
+			brithDate: new Date(brithDate),
+		});
+
+		return this.repository.save(newUser);
+	};
+}
