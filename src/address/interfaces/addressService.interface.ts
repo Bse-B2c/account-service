169321bf20c9e5ff99c1737fb2388db5775b4cde
Rodrigo Limões <rolimoes@gmail.com>
2023-02-28 10:37:28 -0300

    Adding update function declaration in address service interface

--- src/address/interfaces/addressService.interface.ts
+++ src/address/interfaces/addressService.interface.ts
@@ -1,10 +1,12 @@
 import { AddressDto } from '@address/dtos/address.dto';
 import { Address } from '@address/entity/address.entity';
 import { SearchDto } from '@address/dtos/search.dto';
+import { UpdateAddressDto } from '@address/dtos/updateAddress.dto';
 
 export interface AddressService {
 	create(address: AddressDto): Promise<Address>;
 	findOne(id: number): Promise<Address>;
 	find(search: SearchDto): Promise<Array<Address>>;
 	delete(id: number): Promise<Address>;
+	update(id: number, address: UpdateAddressDto): Promise<Address>;
 }
