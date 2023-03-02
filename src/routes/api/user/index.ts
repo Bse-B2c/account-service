a3f137d1eb75cb8ece4e538042e8da05ddabae23
Rodrigo Limões <rolimoes@gmail.com>
2023-03-01 21:38:50 -0300

    Adding new endpoint to find users

--- src/routes/api/user/index.ts
+++ src/routes/api/user/index.ts
@@ -8,13 +8,16 @@ import { userController } from '@src/user';
 // dtos
 import { UserDto } from '@user/dtos/user.dto';
 import { ParamsDto } from '@common/dtos/params.dto';
+import { SearchDto } from '@user/dtos/search.dto';
 
 // validate
 const validateBody = validate('body');
 const validateParams = validate('params');
+const validateQuery = validate('query');
 
 router.post('/', validateBody(UserDto), userController.create);
 router.get('/:id', validateParams(ParamsDto), userController.findOne);
 router.delete('/:id', validateParams(ParamsDto), userController.delete);
+router.get('/', validateQuery(SearchDto), userController.find);
 
 export default router;
