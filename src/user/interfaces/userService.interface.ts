ae4e50e5d768eaa8d6b11613175da38a7df96521
Rodrigo Limões <rolimoes@gmail.com>
2023-03-02 08:32:08 -0300

    Adding find function declaration in user service interface

--- src/user/interfaces/userService.interface.ts
+++ src/user/interfaces/userService.interface.ts
@@ -1,8 +1,10 @@
 import { UserDto } from '@user/dtos/user.dto';
 import { User } from '@user/entity/user.entity';
+import { SearchDto } from '@user/dtos/search.dto';
 
 export interface UserService {
 	create(user: UserDto): Promise<User>;
 	findOne(id: number): Promise<User>;
 	delete(id: number): Promise<User>;
+	find(search: SearchDto): Promise<Array<User>>;
 }
