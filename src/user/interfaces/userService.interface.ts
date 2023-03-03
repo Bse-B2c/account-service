9fa0431d1fe47ea77f02602ed8da88238ce16a60
Rodrigo Limões <rolimoes@gmail.com>
2023-03-02 22:46:15 -0300

    Refactoring findOne function declaration in user service interface;Adding update function declaration in user service interface

--- src/user/interfaces/userService.interface.ts
+++ src/user/interfaces/userService.interface.ts
@@ -1,10 +1,12 @@
 import { UserDto } from '@user/dtos/user.dto';
 import { User } from '@user/entity/user.entity';
 import { SearchDto } from '@user/dtos/search.dto';
+import { FindOptionsSelect } from 'typeorm';
 
 export interface UserService {
 	create(user: UserDto): Promise<User>;
-	findOne(id: number): Promise<User>;
+	findOne(id: number, optionSelect?: FindOptionsSelect<User>): Promise<User>;
 	delete(id: number): Promise<User>;
+	update(id: number, userData: UserDto): Promise<User>;
 	find(search: SearchDto): Promise<Array<User>>;
 }
