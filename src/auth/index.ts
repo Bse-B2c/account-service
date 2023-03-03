2a877dd72a9dc22f09c540797c990359e62e852a
Rodrigo Limões <rolimoes@gmail.com>
2023-03-03 10:37:58 -0300

    implementing the function signIn

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,5 +1,7 @@
 import { AuthController } from '@auth/auth.controller';
 import { AuthService } from '@auth/auth.service';
+import { userService } from '@src/user';
+import { PasswordUtils } from '@common/utils/password.utils';
 
-export const authService = new AuthService();
+export const authService = new AuthService(userService, new PasswordUtils());
 export const authController = new AuthController(authService);
