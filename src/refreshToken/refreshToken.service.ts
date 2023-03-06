a62f3b1a3b9194da6da37a1978fa9ce8ede4c6c4
Rodrigo Limões <rolimoes@gmail.com>
2023-03-05 23:01:36 -0300

    Adding refresh Token service

---
+++ src/refreshToken/refreshToken.service.ts
@@ -0,0 +1,92 @@
+import {
+	RefreshTokenService as Service,
+	option,
+	RefreshTokenInfo,
+} from '@refreshToken/interfaces/refreshTokenService';
+import { sign } from 'jsonwebtoken';
+import { RefreshToken } from '@src/refreshToken/entity/refreshToken.entity';
+import { HttpException, HttpStatusCode } from '@bse-b2c/common';
+import { User } from '@user/entity/user.entity';
+import { Repository } from 'typeorm';
+import { Token } from '@common/interfaces/token';
+import dayjs, { ManipulateType } from 'dayjs';
+
+export class RefreshTokenService implements Service {
+	constructor(private repository: Repository<RefreshToken>) {}
+
+	generateToken = (payload: any, { secret, expiresIn }: option): string => {
+		return sign(payload, secret, {
+			expiresIn,
+		});
+	};
+
+	create = async (user: User): Promise<RefreshTokenInfo> => {
+		const time = Number(process.env['REFRESH_TOKEN_EXPIRES_TIME']) || 20;
+		const unit = process.env['REFRESH_TOKEN_EXPIRES_UNIT'] || 'minute';
+		const expiresIn = dayjs()
+			.add(time, unit as ManipulateType)
+			.unix();
+
+		const newRefreshToken = await this.repository.create({
+			expiresIn,
+			user,
+		});
+
+		await this.repository.save(newRefreshToken);
+
+		return {
+			id: newRefreshToken.id,
+			expiresIn,
+			userId: newRefreshToken.userId,
+		};
+	};
+
+	isExpiredToken = (expiresIn: number) =>
+		dayjs().isAfter(dayjs.unix(expiresIn));
+
+	handle = async (id: number): Promise<Token> => {
+		const refreshToken = await this.repository.findOne({
+			relations: { user: true },
+			where: { id },
+		});
+
+		if (!refreshToken)
+			throw new HttpException({
+				message: 'Refresh token not found',
+				statusCode: HttpStatusCode.NOT_FOUND,
+			});
+
+		const {
+			user: { id: userId, name, roles },
+			expiresIn,
+		} = refreshToken;
+
+		const payload = {
+			name,
+			roles,
+		};
+
+		const token = this.generateToken(payload, {
+			secret: process.env['REFRESH_TOKEN_SECRET'] ?? 'secret',
+			expiresIn,
+		});
+
+		if (this.isExpiredToken(expiresIn)) {
+			await this.deleteMany(userId);
+
+			const newRefreshToken = await this.create(refreshToken.user);
+
+			return { token, refreshToken: newRefreshToken };
+		}
+
+		return { token, refreshToken: undefined };
+	};
+
+	deleteMany = async (userId: number): Promise<Array<RefreshToken>> => {
+		const tokens = await this.repository.find({ where: { userId: userId } });
+
+		if (tokens.length > 0) await this.repository.delete(tokens?.map(e => e.id));
+
+		return tokens;
+	};
+}
