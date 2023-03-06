effc3eadb2469383c381ae6420629eb047ec3cb9
Rodrigo Limões <rolimoes@gmail.com>
2023-03-05 23:03:19 -0300

    Adding new endpoint to refresh token

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import user from '@src/routes/api/user';
 import address from '@src/routes/api/address';
 import auth from '@src/routes/api/auth';
+import token from '@src/routes/api/refreshToken';
 
 const router = Router();
 
 router.use('/auth', auth);
 router.use('/user', user);
 router.use('/address', address);
+router.use('/token', token);
 
 export default router;
