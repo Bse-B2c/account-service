9cd41e680437c69de8b4c8df8ac17b5e6fc69309
Rodrigo Limões <rolimoes@gmail.com>
2023-03-06 22:02:44 -0300

    Adding authenticated middleware in address router

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { Role } from '@common/enums/role.enum';
 import user from '@src/routes/api/user';
 import address from '@src/routes/api/address';
 import auth from '@src/routes/api/auth';
@@ -6,9 +7,20 @@ import token from '@src/routes/api/refreshToken';
 
 const router = Router();
 
+// middleware
+import {
+	ensureAuthenticated,
+	verifyRoles,
+} from '@middleware/ensureAuthenticated';
+
 router.use('/auth', auth);
 router.use('/user', user);
-router.use('/address', address);
+router.use(
+	'/address',
+	ensureAuthenticated,
+	verifyRoles([Role.CONSUMER]),
+	address
+);
 router.use('/token', token);
 
 export default router;
