a43f983acc98f1a4ef0d1f0327c22f2035d5f9ab
Rodrigo Limões <rolimoes@gmail.com>
2023-03-06 21:58:06 -0300

    Adding authenticated middleware

--- src/routes/api/user/index.ts
+++ src/routes/api/user/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { validate } from '@common/utils/validate.utils';
+import { Role } from '@common/enums/role.enum';
 const router = Router();
 
 // controller
@@ -10,15 +11,45 @@ import { UserDto } from '@user/dtos/user.dto';
 import { ParamsDto } from '@common/dtos/params.dto';
 import { SearchDto } from '@user/dtos/search.dto';
 
+// middleware
+import {
+	ensureAuthenticated,
+	verifyRoles,
+} from '@middleware/ensureAuthenticated';
+
 // validate
 const validateBody = validate('body');
 const validateParams = validate('params');
 const validateQuery = validate('query');
 
 router.post('/', validateBody(UserDto), userController.create);
-router.get('/', validateQuery(SearchDto), userController.find);
-router.get('/:id', validateParams(ParamsDto), userController.findOne);
-router.delete('/:id', validateParams(ParamsDto), userController.delete);
-router.put('/:id', validateBody(UserDto), userController.update);
+router.get(
+	'/',
+	ensureAuthenticated,
+	verifyRoles([Role.ADMIN]),
+	validateQuery(SearchDto),
+	userController.find
+);
+router.get(
+	'/:id',
+	ensureAuthenticated,
+	verifyRoles([Role.ADMIN, Role.CONSUMER]),
+	validateParams(ParamsDto),
+	userController.findOne
+);
+router.delete(
+	'/:id',
+	ensureAuthenticated,
+	verifyRoles([Role.ADMIN, Role.CONSUMER]),
+	validateParams(ParamsDto),
+	userController.delete
+);
+router.put(
+	'/:id',
+	ensureAuthenticated,
+	verifyRoles([Role.ADMIN, Role.CONSUMER]),
+	validateBody(UserDto),
+	userController.update
+);
 
 export default router;
